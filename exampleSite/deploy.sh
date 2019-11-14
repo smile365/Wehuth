0570d67a22b85c3acd599e4073cd991384cf6826
songxueyan <sxy9103@qq.com>
2019-11-14 18:57:36 +0800

    增加seobuff

--- exampleSite/deploy.sh
+++ exampleSite/deploy.sh
@@ -1,15 +1,32 @@
-site=/home/myblog
-posts=$site/content/posts
-theme=$site/themes/wehuth
+
+home=/home/www
+blog=$home/blog
+life=$home/live4life
+theme=$home/wehuth
+site=$home/myblog/
+posts=$site/content/posts/
+
+cd $blog
+binfo=$(git pull)
+echo "$binfo"
+
+cd $life
+linfo=$(git pull)
+echo "$linfo"
 
 cd $theme
-tinfo=$(git pull)
+tinfo=pinfo=$(git pull)
+echo "$tinfo"
 
-cd $posts
-pinfo=$(git pull)
+#if [[ $blog == *".."* ]] || [[ $life == *".."* ]] || [[ $theme == *".."* ]]; then
+cp $blog/*.md $posts
+cp $life/*.md $posts
+cp -r $theme $site/themes
+cp $theme/exampleSite/config.toml $site
+cd $site
+hugo
+#fi
 
-if [[ $tinfo == *".."* ]] || [[ $pinfo == *".."* ]]; then
-	cd $site
-	hugo
-fi
-
+# cp -r wehuth quickstart/themes/
+# cp wehuth/exampleSite/config.toml quickstart/
+# hugo server -D -t wehuth --bind=0.0.0.0 --baseURL=http://127.0.0.1:1313
